2a5c65e874cc4029b8c8bd2c26b1ab1b1b5e8dcc
Erik Madrid <emadrid@aptus.io>
2013-07-25 13:43:24 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeployableRailsApp::Application.config.secret_token = 'c269e66acb5bc26edbb97b315bd7f2112609843bc7d921ac5d20962ffc19df7900f9e3b15ddce6a6b133b20298a5949ad10afab0c15edcd5cc09ecbce87c0ec8'
